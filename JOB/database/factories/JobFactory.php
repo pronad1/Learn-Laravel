0fbafdf20d21a626144ee42551ed6c72ac6c6fdb
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-04 08:38:01 +0600

    4/2/25

---
+++ JOB/database/factories/JobFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+
+use App\Models\Employer;
+use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
+ */
+class JobFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->jobTitle(),
+            'employer_id' => Employer::factory(),
+            'salary' => '$500,000 USD'
+        ];
+    }
+}
