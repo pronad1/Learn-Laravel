c5e0afbd31beeaf91dc2ebf69c087f58021af3a5
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-04 08:36:06 +0600

    4/2/25

---
+++ JOB/app/Models/Employer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\User;
+use App\Models\Job;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Gate;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Model::preventLazyLoading();
+
+        
+        // Gate::define('edit-job', function (User $user, Job $job) {
+        //     return $job->employer->user->is($user);
+        // });
+    }
+}
---
+++ JOB/app/Policies/JobPolicy.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Policies;
+
+use Illuminate\Auth\Access\Response;
+use App\Models\Job;
+use App\Models\User;
+use Illuminate\Support\Js;
+
+class JobPolicy
+{
+
+    public function edit(User $user, Job $job) : bool
+    {
+        return $job->employer->user->is($user);
+    }
+
+    // /**
+    //  * Determine whether the user can view any models.
+    //  */
+    // public function viewAny(User $user): bool
+    // {
+    //     return false;
+    // }
+
+    // /**
+    //  * Determine whether the user can view the model.
+    //  */
+    // public function view(User $user, Job $job): bool
+    // {
+    //     return false;
+    // }
+
+    // /**
+    //  * Determine whether the user can create models.
+    //  */
+    // public function create(User $user): bool
+    // {
+    //     return false;
+    // }
+
+    // /**
+    //  * Determine whether the user can update the model.
+    //  */
+    // public function update(User $user, Job $job): bool
+    // {
+    //     return false;
+    // }
+
+    // /**
+    //  * Determine whether the user can delete the model.
+    //  */
+    // public function delete(User $user, Job $job): bool
+    // {
+    //     return false;
+    // }
+
+    // /**
+    //  * Determine whether the user can restore the model.
+    //  */
+    // public function restore(User $user, Job $job): bool
+    // {
+    //     return false;
+    // }
+
+    // /**
+    //  * Determine whether the user can permanently delete the model.
+    //  */
+    // public function forceDelete(User $user, Job $job): bool
+    // {
+    //     return false;
+    // }
+}
