868dd7afe4440fe3643d9d56ea14073d51f83571
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-08 15:29:49 +0600

    2/8/25

--- pixel-position/resources/views/components/layout.blade.php
+++ pixel-position/resources/views/components/layout.blade.php
@@ -4,9 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Pixel-Position</title>
 </head>
 <body>
-    
+    <div>
+        <nav>
+
+            <div>Logo</div>
+
+            <div> Links</div>
+
+            <div>Post a Job</div>
+        </nav>
+
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
 </body>
 </html>
