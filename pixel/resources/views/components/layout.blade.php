13d1070ebaab6b57a1fead4b36cdae9b3e13f9fa
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-14 02:22:58 +0600

    2/13/25

--- pixel/resources/views/components/layout.blade.php
+++ pixel/resources/views/components/layout.blade.php
@@ -1,3 +1,12 @@
-<div>
-    <!-- If you do not have a consistent goal in life, you can not live it in a consistent way. - Marcus Aurelius -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
