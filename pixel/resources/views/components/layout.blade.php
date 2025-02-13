be5f7af874b9942280b9207202a1f0820c008e4b
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-14 02:46:52 +0600

    2/14/25

--- pixel/resources/views/components/layout.blade.php
+++ pixel/resources/views/components/layout.blade.php
@@ -4,9 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pixel</title>
+    <title>Pixel Position</title>
 </head>
 <body>
-    
+    <nav> 
+        <div>
+            <a href="">
+                <img src="{{  }}" alt="">
+            </a>
+        </div>
+        <div>links</div>
+        <div>post a job</div>
+    </nav>
+
+    <main>
+        {{ $slot }} 
+    </main>
 </body>
 </html>
