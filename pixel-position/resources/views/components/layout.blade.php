0dc4c43b28b440fa880216b90d991e744433983d
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-13 22:28:10 +0600

    2/13/25

--- pixel-position/resources/views/components/layout.blade.php
+++ pixel-position/resources/views/components/layout.blade.php
@@ -5,12 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel-Position</title>
+    @vite(['resources/js/app.js'])
 </head>
 <body>
     <div>
         <nav>
 
-            <div>Logo</div>
+            <div>
+                <a href="">
+                    <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
+                </a>
+            </div>
 
             <div> Links</div>
 
