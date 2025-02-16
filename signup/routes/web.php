d714e277a0023846d2aed0b61b1a90a6021e08bd
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-16 14:12:33 +0600

    2/16/25

--- signup/routes/web.php
+++ signup/routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Ensure the SessionController class exists in the specified namespace
+// If it does not exist, create it in the App\Http\Controllers namespace
+
+//Auth
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+// Route::get('/login', [SessionController::class,'create']);
+Route::get('/login', [SessionController::class, 'create'])->name('login');
+Route::post('/login', [SessionController::class,'store']);
